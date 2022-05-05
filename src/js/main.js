0521a3a0bd5a00bacafc1b304f3b3243fc4e1403
Pavel Kogut <kogut.kogut1@mail.ru>
2022-05-05 14:51:42 +0500

    file upload functionality

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Form from './modules/forms';
+import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
     const mainSlider = new MainSlider({btns: '.next', container: '.page'});
@@ -42,5 +44,10 @@ window.addEventListener('DOMContentLoaded', () => {
     new VideoPlayer('.module__video-item .play', '.overlay').init();
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
+
     new Form('.form').init();
+
+    new ShowInfo('.plus').init();
+
+    new Download('.download').init();
 });
